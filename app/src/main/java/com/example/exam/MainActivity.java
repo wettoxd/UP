006d1fcbf9461f5c600c00025c43459f862085e4
wettoxd <dfgfgfgdrrereretr@gmail.com>
2024-04-15 15:45:19 +0300

    first commit

---
+++ app/src/main/java/com/example/exam/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.exam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.window.SplashScreen;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+
+
+
+    }
+
+
+}
