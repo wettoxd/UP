a83ab99d3df0e29200ca434ddd57fcc144af9912
wettoxd <dfgfgfgdrrereretr@gmail.com>
2024-04-17 16:35:22 +0300

    4 commit

--- app/src/main/java/com/example/exam/Sign_up.java
+++ app/src/main/java/com/example/exam/Sign_up.java
@@ -2,7 +2,9 @@ package com.example.exam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Sign_up extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Sign_up extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+    public void log_in(View view) {
+        Intent intent;
+        intent = new Intent(Sign_up.this, Log_in.class);
+        startActivity(intent);
     }
 }
